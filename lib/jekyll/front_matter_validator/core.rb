920b690462aa6512b83220e4ea40e851af53939e
William Brendaw <williambrendaw@icloud.com>
2026-07-26 16:20:04 -0300

    fix: handle case-insensitive extensions and nil match in slug_file type

--- lib/jekyll/front_matter_validator/core.rb
+++ lib/jekyll/front_matter_validator/core.rb
@@ -19,8 +19,8 @@ module Jekyll
       "date" => ->(v) { valid_date?(v) },
       "slug" => ->(v) { v.is_a?(String) && v.match?(SLUG_REGEX) },
       "slug_file" => lambda { |v|
-        v.is_a?(String) && v.match?(/\A(.+)\.[a-z]+\z/) &&
-          Regexp.last_match(1).match?(SLUG_REGEX)
+        v.is_a?(String) && v.match?(/\A(.+)\.[a-z]+\z/i) &&
+          (Regexp.last_match(1)&.match?(SLUG_REGEX) || false)
       }
     }.freeze
 
